63c20cbbea9a5372a68a6450177a52a0f3d77f84
yeny-gomez <yeny-gomez@users.noreply.github.com>
2016-08-17 19:32:24 -0400

    Add files via upload

---
+++ Sin Nombre1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+void main(){
+	
+	int x;
+	long float  y,resta,mult,resul,numero;
+	
+	printf("\n\nIngrese numero: ");
+	 scanf("%i",&numero);
+	
+	y=numero/10;
+	x=numero/10;
+	
+	resta=y-x;
+	mult=resta*10;
+	resul=mult+resta;
+	
+	printf("\n\n %.2f", y);
+	
+}
+
+
+
+
+
+
+
+
